84fea8dd93b8ab36602ab421939a5c52082dec18
noti5-hermel <notificacionhe5@gmail.com>
2026-02-23 11:08:52 -0600

    feat: Enable .env file loading using dotenv-java for application configuration.

--- src/main/java/com/app/performtrackapi/PerformTrackApiApplication.java
+++ src/main/java/com/app/performtrackapi/PerformTrackApiApplication.java
@@ -1,5 +1,6 @@
 package com.app.performtrackapi;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PerformTrackApiApplication {
 
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
         SpringApplication.run(PerformTrackApiApplication.class, args);
     }
-
 }
